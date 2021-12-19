b6639ab151dfaf8dcad1a224c6202f7ba8de5a3b
Soeren Schoenbrod <soeren.schoenbrod@rwth-aachen.de>
2021-12-19 17:21:44 +0100

    Add more precise documentation

--- lib/fast_image_resizer.dart
+++ lib/fast_image_resizer.dart
@@ -3,6 +3,10 @@ library fast_image_resizer;
 import 'dart:ui' as ui;
 import 'dart:typed_data';
 
+/// Resizes the image [rawImage] to [width] and [height].
+///
+/// If only width or height are specified, the resized
+/// image will keep the aspect ratio.
 Future<ByteData?> resizeImage(Uint8List rawImage,
     {int? width, int? height}) async {
   final codec = await ui.instantiateImageCodec(rawImage,
